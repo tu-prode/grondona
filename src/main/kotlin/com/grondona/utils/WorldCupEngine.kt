a220ab556097faa2ea2f1e5c69c0c6a58c54d2be
Cristian Raña <cristian.rana8@gmail.com>
2026-04-08 20:57:35 -0300

    u21 players added

--- src/main/kotlin/com/grondona/utils/WorldCupEngine.kt
+++ src/main/kotlin/com/grondona/utils/WorldCupEngine.kt
@@ -1,11 +1,13 @@
 package com.grondona.utils
 
 import com.grondona.model.Match
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 object WorldCupEngine {
+    val BEST_YOUNG_PLAYER_DATE_LIMIT = LocalDate.parse("2005-01-01")
     val SYSTEM_TOURNAMENT_ID: UUID = UUID.fromString("28652183-a2d6-4f33-a624-0d24645ce3cd")
     const val API_TOURNAMENT_ID: String = "107"
 
