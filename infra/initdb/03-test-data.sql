0da0a3d06104254e3a7d5e03609a28710fbc269c
Cristian Raña <cristian.rana8@gmail.com>
2026-05-20 18:53:23 -0300

    feat: Init.sql updated (#26)

--- infra/initdb/03-test-data.sql
+++ infra/initdb/03-test-data.sql
@@ -17,10 +17,9 @@ INSERT INTO users (id, fullname, username, email, password_hash, permissions) VA
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO groups (id, tournament_id, name, is_private, max_members) VALUES
-    ('f47ac10b-58cc-4372-a567-0e02b2c3d479', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'General', FALSE, 50),
-    ('7c9e6679-7425-40de-944b-e07fc1f90ae7', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'EPO', TRUE, 25),
-    ('b5d4c3a2-1e0f-4d9c-8b7a-6f5e4d3c2b1a', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'Baldosa', TRUE, 27),
-    ('e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'Maldolar', TRUE, 12),
+    ('f47ac10b-58cc-4372-a567-0e02b2c3d479', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'General', FALSE, 100),
+    ('7c9e6679-7425-40de-944b-e07fc1f90ae7', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'Oppa', TRUE, 50),
+    ('b5d4c3a2-1e0f-4d9c-8b7a-6f5e4d3c2b1a', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'Baldosa', TRUE, 30),
     ('8158a607-97b3-47db-8382-92d878358b9c', '28652183-a2d6-4f33-a624-0d24645ce3cd', 'Familia', TRUE, 20)
 ON CONFLICT (id) DO NOTHING;
 
@@ -28,17 +27,16 @@ INSERT INTO group_users (user_id, group_id, role) VALUES
     ('c97ec073-c40c-4094-9f9e-b07074188936', 'f47ac10b-58cc-4372-a567-0e02b2c3d479', 'OWNER'),
     ('c97ec073-c40c-4094-9f9e-b07074188936', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'OWNER'),
     ('c97ec073-c40c-4094-9f9e-b07074188936', 'b5d4c3a2-1e0f-4d9c-8b7a-6f5e4d3c2b1a', 'OWNER'),
-    ('c97ec073-c40c-4094-9f9e-b07074188936', 'e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', 'OWNER'),
     ('c97ec073-c40c-4094-9f9e-b07074188936', '8158a607-97b3-47db-8382-92d878358b9c', 'OWNER'),
     ('60635292-4a13-43d8-b976-b2e292020deb', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'ADMIN'),
+    ('a2618ce3-03a0-4f81-bb0b-010c0245a65b', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'ADMIN'),
     ('4fc682de-233f-4b0f-b4c3-4ee0f5716675', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
     ('56118705-5d57-4a6d-9f38-46606c78dbd6', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
     ('ef7aacbb-e1f8-46eb-bbd9-21dafe041749', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
     ('b49d0241-d664-4223-ba33-d448ec050abe', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
-    ('a2618ce3-03a0-4f81-bb0b-010c0245a65b', 'e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', 'ADMIN'),
-    ('2b67aaa9-2ecf-4d21-9ce1-e378337b6adb', 'e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', 'MEMBER'),
-    ('b7d358aa-42c0-4b22-9da2-ed292a00ee47', 'e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', 'MEMBER'),
-    ('6baadbd8-dd3e-4926-8de6-e5908f774c4e', 'e8d7c6b5-a4f3-4e2d-9c1b-0a8f7e6d5c4b', 'MEMBER'),
+    ('2b67aaa9-2ecf-4d21-9ce1-e378337b6adb', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
+    ('b7d358aa-42c0-4b22-9da2-ed292a00ee47', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
+    ('6baadbd8-dd3e-4926-8de6-e5908f774c4e', '7c9e6679-7425-40de-944b-e07fc1f90ae7', 'MEMBER'),
     ('4cb252f7-9dac-4249-a9a2-b45d5234d384', 'b5d4c3a2-1e0f-4d9c-8b7a-6f5e4d3c2b1a', 'ADMIN'),
     ('bdbf29ee-cfda-4d02-9928-af93ebd40892', 'b5d4c3a2-1e0f-4d9c-8b7a-6f5e4d3c2b1a', 'MEMBER'),
     ('4a97633c-649b-4315-91a9-f995dc950171', '8158a607-97b3-47db-8382-92d878358b9c', 'ADMIN'),
